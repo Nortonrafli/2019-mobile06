1b48b4c6dd0a5111e0d9418f2b1ec369a9d99317
nortonrafli <norton.nra27@gmail.com>
2019-11-10 23:51:24 +0700

    commit fragment result

--- app/src/main/java/id/ac/polinema/idealbodyweight/fragments/ResultFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragments/ResultFragment.java
@@ -1,7 +1,6 @@
 package id.ac.polinema.idealbodyweight.fragments;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import id.ac.polinema.idealbodyweight.R;
 
@@ -17,18 +18,10 @@ import id.ac.polinema.idealbodyweight.R;
  * Activities that contain this fragment must implement the
  * {@link ResultFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link ResultFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class ResultFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String information;
 
     private OnFragmentInteractionListener mListener;
 
@@ -36,46 +29,31 @@ public class ResultFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ResultFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ResultFragment newInstance(String param1, String param2) {
-        ResultFragment fragment = new ResultFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public void setInformation(String information) {
+        this.information = information;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false);
+        View view = inflater.inflate(R.layout.fragment_result, container, false);
+        TextView informationText = view.findViewById(R.id.text_information);
+        informationText.setText(information);
+        Button tryAgainButton = view.findViewById(R.id.button_try_again);
+        tryAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.onTryAgainButtonClicked("BrocaIndex");
+                }
+            }
+        });
+        return  view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
@@ -106,6 +84,6 @@ public class ResultFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onTryAgainButtonClicked(String tag);
     }
 }
