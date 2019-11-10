1277c0192f20bbe5ecd088634d80469ad0425a25
nortonrafli <norton.nra27@gmail.com>
2019-11-11 00:00:57 +0700

    commit feagment brocaindex fragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/fragments/BrocaIndexFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragments/BrocaIndexFragment.java
@@ -1,34 +1,29 @@
 package id.ac.polinema.idealbodyweight.fragments;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import id.ac.polinema.idealbodyweight.R;
+import id.ac.polinema.idealbodyweight.util.BrocaIndex;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link BrocaIndexFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link BrocaIndexFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class BrocaIndexFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -36,47 +31,37 @@ public class BrocaIndexFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BrocaIndexFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BrocaIndexFragment newInstance(String param1, String param2) {
-        BrocaIndexFragment fragment = new BrocaIndexFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_broca_index, container, false);
+        View view = inflater.inflate(R.layout.fragment_broca_index, container, false);
+        final RadioGroup genderGroup = view.findViewById(R.id.group_gender);
+        final EditText heightText  = view.findViewById(R.id.input_height);
+
+        Button calculateButton = view.findViewById(R.id.button_calculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    String heightString = heightText.getText().toString();
+                    int checkedId = genderGroup.getCheckedRadioButtonId();
+                    if ((checkedId != -1) && !TextUtils.isEmpty(heightString)) {
+                        int height = Integer.parseInt(heightString);
+                        int gender = (checkedId == R.id.radio_male) ? BrocaIndex.MALE : BrocaIndex.FEMALE;
+                        BrocaIndex brocaIndex = new BrocaIndex(gender, height);
+                        mListener.onCalculateBrocaIndexClicked(brocaIndex.getIndex());
+                    } else {
+                        Toast.makeText(getActivity(), "Please select gender and input your height", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -106,6 +91,6 @@ public class BrocaIndexFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onCalculateBrocaIndexClicked(float index);
     }
 }
